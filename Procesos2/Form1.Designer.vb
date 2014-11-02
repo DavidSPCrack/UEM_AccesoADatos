8519af70efaa2d49e6ae9d70a7b2f70ea5e16ead
David <kroneos@gmail.com>
2014-11-02 17:03:55 +0100

    qqq

--- Procesos2/Form1.Designer.vb
+++ Procesos2/Form1.Designer.vb
@@ -22,17 +22,163 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Start1 = New System.Windows.Forms.Button()
+        Me.Stop1 = New System.Windows.Forms.Button()
+        Me.Stop2 = New System.Windows.Forms.Button()
+        Me.Start2 = New System.Windows.Forms.Button()
+        Me.Stop3 = New System.Windows.Forms.Button()
+        Me.Start3 = New System.Windows.Forms.Button()
+        Me.Start4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'Start1
+        '
+        Me.Start1.Location = New System.Drawing.Point(10, 16)
+        Me.Start1.Name = "Start1"
+        Me.Start1.Size = New System.Drawing.Size(95, 27)
+        Me.Start1.TabIndex = 0
+        Me.Start1.Text = "Iniciar hilo 1"
+        Me.Start1.UseVisualStyleBackColor = True
+        '
+        'Stop1
+        '
+        Me.Stop1.Enabled = False
+        Me.Stop1.Location = New System.Drawing.Point(111, 16)
+        Me.Stop1.Name = "Stop1"
+        Me.Stop1.Size = New System.Drawing.Size(95, 27)
+        Me.Stop1.TabIndex = 1
+        Me.Stop1.Text = "Parar hilo 1"
+        Me.Stop1.UseVisualStyleBackColor = True
+        '
+        'Stop2
+        '
+        Me.Stop2.Enabled = False
+        Me.Stop2.Location = New System.Drawing.Point(111, 73)
+        Me.Stop2.Name = "Stop2"
+        Me.Stop2.Size = New System.Drawing.Size(95, 27)
+        Me.Stop2.TabIndex = 3
+        Me.Stop2.Text = "Parar hilo 2"
+        Me.Stop2.UseVisualStyleBackColor = True
+        '
+        'Start2
+        '
+        Me.Start2.Location = New System.Drawing.Point(10, 73)
+        Me.Start2.Name = "Start2"
+        Me.Start2.Size = New System.Drawing.Size(95, 27)
+        Me.Start2.TabIndex = 2
+        Me.Start2.Text = "Iniciar hilo 2"
+        Me.Start2.UseVisualStyleBackColor = True
+        '
+        'Stop3
+        '
+        Me.Stop3.Enabled = False
+        Me.Stop3.Location = New System.Drawing.Point(111, 128)
+        Me.Stop3.Name = "Stop3"
+        Me.Stop3.Size = New System.Drawing.Size(95, 27)
+        Me.Stop3.TabIndex = 5
+        Me.Stop3.Text = "Parar hilo 3"
+        Me.Stop3.UseVisualStyleBackColor = True
+        '
+        'Start3
+        '
+        Me.Start3.Location = New System.Drawing.Point(10, 128)
+        Me.Start3.Name = "Start3"
+        Me.Start3.Size = New System.Drawing.Size(95, 27)
+        Me.Start3.TabIndex = 4
+        Me.Start3.Text = "Iniciar hilo 3"
+        Me.Start3.UseVisualStyleBackColor = True
+        '
+        'Start4
+        '
+        Me.Start4.Location = New System.Drawing.Point(10, 188)
+        Me.Start4.Name = "Start4"
+        Me.Start4.Size = New System.Drawing.Size(95, 27)
+        Me.Start4.TabIndex = 6
+        Me.Start4.Text = "Iniciar hilo 4"
+        Me.Start4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(266, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "0"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(212, 73)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(297, 26)
+        Me.ProgressBar1.TabIndex = 8
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(212, 128)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(91, 27)
+        Me.TextBox1.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(309, 128)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(91, 27)
+        Me.TextBox2.TabIndex = 10
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(406, 128)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(91, 27)
+        Me.TextBox3.TabIndex = 11
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(565, 352)
+        Me.ClientSize = New System.Drawing.Size(521, 240)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Start4)
+        Me.Controls.Add(Me.Stop3)
+        Me.Controls.Add(Me.Start3)
+        Me.Controls.Add(Me.Stop2)
+        Me.Controls.Add(Me.Start2)
+        Me.Controls.Add(Me.Stop1)
+        Me.Controls.Add(Me.Start1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Conceptos básicos"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Start1 As System.Windows.Forms.Button
+    Friend WithEvents Stop1 As System.Windows.Forms.Button
+    Friend WithEvents Stop2 As System.Windows.Forms.Button
+    Friend WithEvents Start2 As System.Windows.Forms.Button
+    Friend WithEvents Stop3 As System.Windows.Forms.Button
+    Friend WithEvents Start3 As System.Windows.Forms.Button
+    Friend WithEvents Start4 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
 
 End Class
--- Procesos2/Form1.vb
+++ Procesos2/Form1.vb
@@ -1,3 +1,191 @@
-﻿Public Class Form1
+﻿Imports System.Threading
 
+Public Class Form1
+
+#Region "Eventos de Hilo y sus Handlers"
+
+    Private Event InicioHilo(ByVal Inicio As DateTime, ByVal Hilo As Integer)
+    Private Event FinHilo(ByVal Fin As DateTime, ByVal Hilo As Integer)
+
+    Private HilosWorking As Boolean() = {False, False, False, False}
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddHandler InicioHilo, AddressOf InicioHiloSub
+        AddHandler FinHilo, AddressOf FinHiloSub
+
+        CheckForIllegalCrossThreadCalls = False
+
+    End Sub
+
+    Private Sub InicioHiloSub(ByVal Inicio As DateTime, ByVal Hilo As Integer)
+        HilosWorking(Hilo) = True
+    End Sub
+
+    Private Sub FinHiloSub(ByVal Inicio As DateTime, ByVal Hilo As Integer)
+        HilosWorking(Hilo) = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
+
+#Region "Hilo 1"
+
+    Private Sub PararHilo1()
+        Stop1.Enabled = False
+        RaiseEvent FinHilo(DateTime.Now, 0)
+        Start1.Enabled = True
+    End Sub
+
+    Private Sub InicializarHilo1()
+        Start1.Enabled = False
+        Dim hilo As New Thread(AddressOf Hilo1Method)
+        hilo.Name = "Hilo1"
+        hilo.IsBackground = True
+        hilo.Start()
+        Stop1.Enabled = True
+    End Sub
+
+    Private Sub Hilo1Method()
+        RaiseEvent InicioHilo(DateTime.Now, 0)
+        While HilosWorking(0)
+            Dim num As Integer = Label1.Text
+            num += 1
+            Label1.Text = num
+            Thread.Sleep(1000)
+        End While
+    End Sub
+
+    Private Sub Start1_Click(sender As Object, e As EventArgs) Handles Start1.Click
+        If Not HilosWorking(0) Then
+            InicializarHilo1()
+        End If
+    End Sub
+
+    Private Sub Stop1_Click(sender As Object, e As EventArgs) Handles Stop1.Click
+        If HilosWorking(0) Then
+            PararHilo1()
+        End If
+    End Sub
+
+#End Region
+#Region "Hilo 2"
+    Private Sub PararHilo2()
+        Stop2.Enabled = False
+        RaiseEvent FinHilo(DateTime.Now, 1)
+        Start2.Enabled = True
+    End Sub
+
+    Private Sub InicializarHilo2()
+        Start2.Enabled = False
+        Dim hilo As New Thread(AddressOf Hilo2Method)
+        hilo.Name = "Hilo2"
+        hilo.IsBackground = True
+        hilo.Start()
+        Stop2.Enabled = True
+    End Sub
+
+    Private Sub Hilo2Method()
+        RaiseEvent InicioHilo(DateTime.Now, 1)
+        While HilosWorking(1)
+            Dim num As Integer = ProgressBar1.Value
+            If num > 99 Then
+                num = 0
+            End If
+            num += 1
+            ProgressBar1.Value = num
+            If num = 100 Then
+                Thread.Sleep(1000)
+            End If
+            Thread.Sleep(50)
+        End While
+    End Sub
+    Private Sub Start2_Click(sender As Object, e As EventArgs) Handles Start2.Click
+        If Not HilosWorking(1) Then
+            InicializarHilo2()
+        End If
+    End Sub
+
+    Private Sub Stop2_Click(sender As Object, e As EventArgs) Handles Stop2.Click
+        If HilosWorking(1) Then
+            PararHilo2()
+        End If
+    End Sub
+
+#End Region
+#Region "Hilo 3"
+    Private Sub PararHilo3()
+        Stop3.Enabled = False
+        RaiseEvent FinHilo(DateTime.Now, 2)
+        Start3.Enabled = True
+    End Sub
+
+    Private Sub InicializarHilo3()
+        Start3.Enabled = False
+        Dim hilo As New Thread(AddressOf Hilo3Method)
+        hilo.Name = "Hilo3"
+        hilo.IsBackground = True
+        hilo.Start()
+        Stop3.Enabled = True
+    End Sub
+
+    Private Sub Hilo3Method()
+        RaiseEvent InicioHilo(DateTime.Now, 2)
+        While HilosWorking(2)
+            TextBox1.BackColor = GetRandomColor(1)
+            TextBox2.BackColor = GetRandomColor(2)
+            TextBox3.BackColor = GetRandomColor(3)
+            Thread.Sleep(500)
+        End While
+    End Sub
+
+    Private Function GetRandomColor(ByVal num As Integer)
+        Dim rnd1 As New Random(CInt(Date.Now.Ticks Mod Integer.MaxValue) + num * 1337)
+        Dim mColor As Color
+
+        mColor = Color.FromArgb(rnd1.Next(0, 256), rnd1.Next(0, 256), rnd1.Next(0, 256))
+        Return mColor
+    End Function
+    Private Sub Start3_Click(sender As Object, e As EventArgs) Handles Start3.Click
+        If Not HilosWorking(2) Then
+            InicializarHilo3()
+        End If
+    End Sub
+
+    Private Sub Stop3_Click(sender As Object, e As EventArgs) Handles Stop3.Click
+        If HilosWorking(2) Then
+            PararHilo3()
+        End If
+    End Sub
+
+#End Region
+#Region "Hilo 4"
+
+    Private Sub PararHilo4()
+        RaiseEvent FinHilo(DateTime.Now, 3)
+        Start4.Enabled = True
+    End Sub
+    Private Sub InicializarHilo4()
+        Start4.Enabled = False
+        Dim hilo As New Thread(AddressOf Hilo4Method)
+        hilo.Name = "Hilo4"
+        hilo.IsBackground = True
+        hilo.Start()
+    End Sub
+
+    Private Sub Hilo4Method()
+        RaiseEvent InicioHilo(DateTime.Now, 3)
+        While HilosWorking(3)
+            MsgBox("Ejecución del hilo 4")
+            PararHilo4()
+        End While
+    End Sub
+    Private Sub Start4_Click(sender As Object, e As EventArgs) Handles Start4.Click
+        If Not HilosWorking(3) Then
+            InicializarHilo4()
+        End If
+    End Sub
+#End Region
 End Class
