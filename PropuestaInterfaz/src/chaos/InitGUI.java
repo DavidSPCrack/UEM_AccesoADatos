99cb2738589f15df7ae0d5e3a6e9fccbc49fffb1
David Sancho <kroneos@gmail.com>
2014-12-05 16:08:58 +0100

    commit prop

--- PropuestaInterfaz/src/chaos/InitGUI.java
+++ PropuestaInterfaz/src/chaos/InitGUI.java
@@ -7,6 +7,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JTabbedPane;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.ListSelectionModel;
 
 public class InitGUI extends JFrame {
 
@@ -43,12 +52,83 @@ public class InitGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public InitGUI() {
+		setTitle("Travel Agency");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JPanel panel_6 = new JPanel();
+		panel.add(panel_6);
+		panel_6.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JPanel panel_7 = new JPanel();
+		panel_6.add(panel_7);
+		panel_7.setLayout(new BoxLayout(panel_7, BoxLayout.Y_AXIS));
+		
+		JLabel label = new JLabel("Travel to...");
+		panel_7.add(label);
+		
+		Component verticalStrut_1 = Box.createVerticalStrut(5);
+		panel_7.add(verticalStrut_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_6.add(panel_3);
+		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.Y_AXIS));
+		
+		JLabel lblTravelTo = new JLabel("Travel to...");
+		panel_3.add(lblTravelTo);
+		
+		Component verticalStrut = Box.createVerticalStrut(5);
+		panel_3.add(verticalStrut);
+		
+		JPanel panel_5 = new JPanel();
+		panel.add(panel_5);
+		panel_5.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JList list = new JList();
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Madrid", "Barcelona", "Londres", "Paris", "Berlin"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		panel_5.add(list);
+		
+		JList list_1 = new JList();
+		list_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list_1.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Madrid", "Barcelona", "Londres", "Paris", "Berlin"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		panel_5.add(list_1);
+		
+		JPanel panel_4 = new JPanel();
+		panel.add(panel_4);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Detail", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("Views", null, panel_2, null);
 	}
 
 }
